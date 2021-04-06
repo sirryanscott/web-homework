c5b268662193f7da76e88876212134d822f238e1
Ryan <ryan.s.struthers@gmail.com>
2021-04-06 14:22:53 -0600

    test data for db

--- elixir/priv/repo/seeds.exs
+++ elixir/priv/repo/seeds.exs
@@ -9,3 +9,96 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Homework.Users.User
+alias Homework.Transactions.Transaction
+alias Homework.Merchants.Merchant
+
+#
+# populate User table
+#
+Homework.Repo.insert!(%User{
+    id: "a0bbd2a1-dac7-427e-9d92-38e476bb832e",
+    dob: "04/03/1972",
+    first_name: "Thomas",
+    last_name: "Clark"
+})
+Homework.Repo.insert!(%User{
+    dob: "07/04/1994",
+    first_name: "Paul",
+    last_name: "Johnson"
+})
+Homework.Repo.insert!(%User{
+    dob: "10/17/1989",
+    first_name: "Jordan",
+    last_name: "Witcomb"
+})
+Homework.Repo.insert!(%User{
+    dob: "09/07/1965",
+    first_name: "Bjorn",
+    last_name: "Borgensson"
+})
+
+
+##
+## populate Merchant table
+##
+Homework.Repo.insert!(%Merchant{
+    name: "Costco",
+    description: "Costco Warehouse"
+})
+Homework.Repo.insert!(%Merchant{
+    id: "a0bbd2a1-dac7-427e-9d92-38e476bb832d",
+    name: "Walmart",
+    description: "Walmart Supercenter"
+})
+Homework.Repo.insert!(%Merchant{
+    name: "Home Depot",
+    description: "Hardware Store"
+})
+
+
+#
+# populate Transaction table
+#
+Homework.Repo.insert!(%Transaction{
+    amount: 100, 
+    credit: false,
+    debit: true,
+    description: "clothes",
+    merchant_id: "a0bbd2a1-dac7-427e-9d92-38e476bb832d",
+    user_id: "a0bbd2a1-dac7-427e-9d92-38e476bb832e",
+})
+Homework.Repo.insert!(%Transaction{
+    amount: 15,
+    credit: false,
+    debit: true,
+    description: "movie tickets",
+    merchant_id: "a0bbd2a1-dac7-427e-9d92-38e476bb832d",
+    user_id: "a0bbd2a1-dac7-427e-9d92-38e476bb832e",
+})
+Homework.Repo.insert!(%Transaction{
+    amount: 4567,
+    credit: false,
+    debit: true,
+    description: "grocery visit",
+    merchant_id: "a0bbd2a1-dac7-427e-9d92-38e476bb832d",
+    user_id: "a0bbd2a1-dac7-427e-9d92-38e476bb832e",
+})
+Homework.Repo.insert!(%Transaction{
+    amount: 2,
+    credit: true,
+    debit: false,
+    description: "paying back for ice cream cone",
+    merchant_id: "a0bbd2a1-dac7-427e-9d92-38e476bb832d",
+    user_id: "a0bbd2a1-dac7-427e-9d92-38e476bb832e",
+})
+Homework.Repo.insert!(%Transaction{
+    amount: 3726,
+    credit: true,
+    debit: false,
+    description: "return for that broken item",
+    merchant_id: "a0bbd2a1-dac7-427e-9d92-38e476bb832d",
+    user_id: "a0bbd2a1-dac7-427e-9d92-38e476bb832e",
+})
+
